36b5bf5e72d39f8d64e8969bbedf2aa4052285ac
sgr-nz <github@capaxsolutions.co.uk>
2024-04-03 15:12:33 +1300

    Fetching the collection and token data from index.json rather than individual files Removed tokenMap and the md5 fen

--- src/stores/asset-store.ts
+++ src/stores/asset-store.ts
@@ -1,4 +1,3 @@
-import { md5 } from 'md5js';
 import { defineStore } from 'pinia';
 
 interface Metadata {
@@ -15,39 +14,34 @@ interface Metadata {
   moves: number;
 }
 
-interface TokenMap {
-  [fenString: string]: string; // Map FEN string to token_id
-}
-
 export const useAssetStore = defineStore('asset', {
   state: () => ({
     metadata: null as Metadata | null,
-    tokenMap: {} as TokenMap,
+    tokens: null as Metadata[] | null,
     loading: true, // Add a loading state
     collection_id: '' as string,
     collection_name: '' as string,
     collection_icon_url: '' as string,
   }),
   actions: {
-    async loadCollection(collection_id: string) {
+    async loadCollection() {
       this.loading = true;
-      if (collection_id === this.collection_id) return; // Don't load collection data if already loaded
-      if (collection_id === '0') return; // Don't load collection data for 0
-
+      if (this.tokens) return; // Don't load collection if already loaded
+      const indexFile = `${process.env.NEVERNERF_METADATA_API_URL}/index.json`;
       try {
-        const collectionConfigResponse = await fetch(`/${collection_id}.json`);
+        const collectionConfigResponse = await fetch(indexFile);
         if (!collectionConfigResponse.ok) {
           throw new Error(
-            `Network response was not ok for collection: ${collection_id}`
+            `Network response was not ok for collection: ${indexFile}`
           );
         }
         const collectionConfig = await collectionConfigResponse.json();
-        this.tokenMap = collectionConfig.tokenMap || {};
-        this.collection_id = collectionConfig.collection.id;
-        this.collection_name = collectionConfig.collection.name;
-        this.collection_icon_url = collectionConfig.collection.icon_url;
+        this.tokens = collectionConfig.tokens || {};
+        this.collection_id = collectionConfig.collection_id;
+        this.collection_name = collectionConfig.collection_name;
+        this.collection_icon_url = collectionConfig.collection_icon_url;
 
-        console.log('Data loaded for collection:', collection_id);
+        console.log(`${this.tokens?.length} loaded for collection.`);
       } catch (error) {
         console.error('Error loading collection:', error);
       }
@@ -55,31 +49,45 @@ export const useAssetStore = defineStore('asset', {
     },
     async loadMetadata(token_id: string) {
       this.loading = true;
-      if (token_id === '0') {
-        this.loading = false;
-        return; // Don't load metadata for the 0 token
-      }
 
+      // Don't load metadata if token_id missing
       if (!token_id) {
         this.loading = false;
         console.error('TokenID not provided');
         return;
       }
 
+      // Don't load metadata if collection index missing
+      if (!this.tokens) {
+        this.loading = false;
+        return;
+      }
+
+      // Don't load metadata for the 0 token
+      if (token_id === '0') {
+        this.loading = false;
+        return;
+      }
+
+      // Avoid reloading if data is already present
       const current_token_id = this.metadata?.token_id;
       if (this.metadata && current_token_id === token_id) {
         this.loading = false;
-        return; // Avoid reloading if data is already present
+        return;
       }
+
       try {
         //Load token metadata
-        const metadataApiUrl = `${process.env.NEVERNERF_METADATA_API_URL}/${token_id}`;
-        console.log(metadataApiUrl);
-        const response = await fetch(metadataApiUrl);
-        if (!response.ok) {
-          throw new Error(`Network response was not ok for token: ${token_id}`);
+        const index = this.tokens.findIndex(
+          (item) => item.token_id === token_id
+        );
+        if (index !== -1) {
+          console.log(`Switching token context to: ${token_id}`);
+          this.metadata = this.tokens[index];
+        } else {
+          console.log(`No token found for: ${token_id}`);
+          return;
         }
-        this.metadata = await response.json();
       } catch (error) {
         console.error('Error loading metadata:', error);
         this.metadata = null; // Only reset if this is intended on error
@@ -87,8 +95,20 @@ export const useAssetStore = defineStore('asset', {
       this.loading = false;
     },
     getTokenIdByFen(fenString: string): string | undefined {
-      const fenStringHash = md5(fenString, 32).substring(0, 8);
-      return this.tokenMap[fenStringHash];
+      if (!this.tokens) return;
+      try {
+        //Load token metadata
+        const index = this.tokens?.findIndex((item) => item.fen === fenString);
+        if (index !== -1) {
+          return this.tokens[index].token_id;
+        } else {
+          console.log(`No token found for FEN: ${fenString}`);
+          return;
+        }
+      } catch (error) {
+        console.error(`Error loading token via FEN:${fenString}`, error);
+        this.metadata = null; // Only reset if this is intended on error
+      }
     },
   },
 });
